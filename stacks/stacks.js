e1d0fcfba25edb81806d6c94f1e528079427162b
JefferLindao <jefferlindao@gmail.com>
2021-03-28 12:37:46 -0500

    Construyendo un Stack

---
+++ stacks/stacks.js
@@ -0,0 +1,34 @@
+class Nodo {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor() {
+    this.top = null;
+    this.botton = null;
+    this.length = 0;
+  }
+
+  peek() {
+    return this.top;
+  }
+
+  push(value) {
+    const newNode = new Node(value);
+    if (this.length == 0) {
+      this.top = newNode;
+      this.botton = newNode
+    } else {
+      const holdingPointer = this.top;
+      this.top = newNode;
+      this.top.next = holdingPointer;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+let myStack = new Stack();
