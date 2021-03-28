f66b98625fba9eacd61faa003631c3f360bc5b8b
JefferLindao <jefferlindao@gmail.com>
2021-03-27 21:58:35 -0500

    Implementando el metodo get

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -18,6 +18,19 @@ class HashTable {
     this.data[adrress].push([key, value]);
     return this.data;
   }
+
+  get(key) {
+    const address = this.hashMethod(key);
+    const currentBucket = this.data[adrress];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
 }
 
 const myHashTable = new HashTable(50);
